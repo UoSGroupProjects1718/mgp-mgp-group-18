0e20ae708b27a804250cbf87a3bbea1f44c6a8fa
jackfuller1996 <s176802@ucs.ac.uk>
2018-02-20 17:55:09 +0000

    Loser or winner
    there is now a decided win or lose system

---
+++ Game/Group 18 Proto/Assets/Player_2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_2 : MonoBehaviour {
+
+    public BoxCollider2D WinnersBox1;
+    public bool IsWinner;
+    public SpriteRenderer Win;
+    public SpriteRenderer Lose;
+    public bool Player1;
+
+	// Use this for initialization
+	void Start () {
+        Win.enabled = false;
+        Lose.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Player1 = (GameObject.Find("Player").GetComponent<Winner>().result);
+    }
+
+    public void OnTriggerEnter2D(Collider2D WinnersBox)
+    {
+        if (WinnersBox.gameObject.tag == "Ground")
+        {
+            if (Player1 == true)
+            {
+                Lose.enabled = true;
+            }
+            else
+            {
+                Win.enabled = true;
+                IsWinner = true;
+            }
+        }
+    }
+}
---
+++ Game/Group 18 Proto/Assets/Scripts/Winner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Winner : MonoBehaviour
+{
+
+    public BoxCollider2D WinnersBox;
+    public SpriteRenderer Win;
+    public SpriteRenderer Lose;
+    public bool Player2;
+    public bool result;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Win.enabled = false;
+        Lose.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Player2 = (GameObject.Find("Player2").GetComponent<Player_2>().IsWinner);
+    }
+    public void OnTriggerEnter2D(Collider2D WinnersBox)
+    {
+        if (WinnersBox.gameObject.tag == "Ground")
+        {
+            if (Player2 == true)
+            {
+                Lose.enabled = true;
+            }
+            else
+            {
+                Win.enabled = true;
+                result = true;
+            }
+        }
+    }
+}
