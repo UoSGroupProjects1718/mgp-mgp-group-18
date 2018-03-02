c5ed607ccf202d04db2d7c56eec9954190b884c5
jackfuller1996 <s176802@ucs.ac.uk>
2018-03-02 04:22:27 +0000

    Changed a bit

--- Game/Group 18 Proto/Assets/Scripts/Destory.cs
+++ Game/Group 18 Proto/Assets/Scripts/Destory.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Destory : MonoBehaviour
 {
     void OnMouseDown()
-    {
-        Destroy(gameObject);
-    }
-
+        {
+            Destroy(gameObject);
+        }
 }
---
+++ Game/Group 18 Proto/Assets/Turns.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turns : MonoBehaviour {
+
+    public bool Player1Turn;
+    public BoxCollider2D P1Box;
+    public BoxCollider2D P2Box;
+    private bool Turn;
+
+	// Use this for initialization
+	void Start () {
+        Player1Turn = true;
+        Turn = GetComponent<BoxCollider2D>().isTrigger;
+        Turn = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        if (Player1Turn == true)
+        {
+            
+        }
+
+    }
+}
