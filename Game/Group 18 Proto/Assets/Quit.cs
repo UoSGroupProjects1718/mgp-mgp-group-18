7a5f17a8001c7d0545509d54a575f3296aadf413
jackfuller1996 <s176802@ucs.ac.uk>
2018-04-17 21:43:32 +0100

    Menu
    Menu has been implemented

---
+++ Game/Group 18 Proto/Assets/Quit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Quit : MonoBehaviour {
+
+    public Button QuitButton;
+
+    void Start()
+    {
+        Button btn = QuitButton.GetComponent<Button>();
+        btn.onClick.AddListener(AQuit);
+    }
+    void AQuit()
+    {
+        Application.Quit();
+    }
+}
